0374bd02d948bda059c55ab0c7eeb8856ceb8cee
David Amores <davidamores3@gmail.com>
2018-08-02 00:15:48 +1000

    Retrieving alerts from db

--- evacuation/models.py
+++ evacuation/models.py
@@ -2,6 +2,34 @@ from django.db import models
 
 
 class Notification(models.Model):
-    text = models.TextField()
-    type = models.CharField(max_length=20)
-    date = models.DateTimeField('date sent')
+    title = models.CharField(max_length=200)
+    description = models.TextField(default="")
+    TYPE_CHOICES = (
+        ('Alert', 'Alert'),
+        ('Warning', 'Warning'),
+        ('Info', 'Info'),
+        ('Success', 'Success'),
+    )
+    type = models.CharField(
+        max_length=20,
+        choices=TYPE_CHOICES,
+        default='Alert',
+    )
+    date = models.DateTimeField('date sent', auto_now_add=True)
+    read = models.BooleanField(default=False)
+    ICON_CLASSES = {
+        'Alert': 'fa-exclamation-circle bg-danger',
+        'Warning': 'fa-warning bg-warning',
+        'Info': 'fa-info-circle bg-info',
+        'Success': 'fa-check bg-flat-color-5',
+    }
+    icon_class = ""
+
+    @classmethod
+    def from_db(cls, db, field_names, values):
+        instance = super().from_db(db, field_names, values)
+        instance.icon_class = instance.ICON_CLASSES[instance.type]
+        return instance
+
+    def __str__(self):
+        return '%s (%s)' % (self.title, self.date)
--- evacuation/views.py
+++ evacuation/views.py
@@ -17,6 +17,10 @@ def building_map(request):
 
 
 def alerts(request):
-    last_notification_list = Notification.objects.order_by('-date')[:5]
-    context = {'last_notification_list': last_notification_list}
+    unread_notifications = Notification.objects.filter(read=False).order_by('-date')
+    read_notifications = Notification.objects.filter(read=True).order_by('-date')[:5]
+    context = {
+        'unread_notifications': unread_notifications,
+        'read_notifications': read_notifications,
+    }
     return render(request, 'evacuation/alerts.html', context)
