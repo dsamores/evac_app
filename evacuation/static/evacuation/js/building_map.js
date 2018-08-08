579d7778340274c9114d2bb7d1e9796d526fb4da
David Amores <davidamores3@gmail.com>
2018-08-08 21:00:40 +1000

    Can choose initial point

--- evacuation/static/evacuation/js/building_map.js
+++ evacuation/static/evacuation/js/building_map.js
@@ -29,7 +29,37 @@ function Place(mwPlace){
                     self.exitRoutes.push(directions);
                     if(self.exitRoutes.length >= 4){
                         self.sortRoutes();
-                        map.startDirections(self.exitRoutes[1]);
+                        map.startDirections(self.exitRoutes[0]);
+                    }
+                }
+            });
+        }
+    }
+}
+
+function Location(lat, lng){
+    this.lat = lat;
+    this.lng = lng;
+    this.exitRoutes = new Array();
+
+    this.sortRoutes = function(){
+        this.exitRoutes.sort(function(er1, er2){
+            return er1.distance - er2.distance;
+        });
+    }
+
+    this.getRoutesAndDisplay = function(){
+        var self = this;
+        for(exitId in exitIds){
+            Mapwize.Api.getDirections({latitude: self.lat, longitude: self.lng, floor: 1}, {placeId: exitIds[exitId]}, null, null, function(err, directions){
+                if (err) {
+                    console.error('An error occur during direction fetching', err);
+                }
+                else {
+                    self.exitRoutes.push(directions);
+                    if(self.exitRoutes.length >= 4){
+                        self.sortRoutes();
+                        map.startDirections(self.exitRoutes[0]);
                     }
                 }
             });
@@ -54,13 +84,22 @@ $(document).ready(function($) {
     });
 
     map.on('placeClick', function (e) {
-        //console.log(e.place);
         if(e.place.placeType.name != 'ISO7010-E001' && e.place.placeType.name != 'ISO7010-E002'){
             map.stopDirections();
             var place = new Place(e.place);
             place.getRoutesAndDisplay();
-            //map.showDirections({placeId: e.place._id}, {placeId: '5b68fc6e7c31b70004d8b26a'});
-
         }
+    });
+
+    map.on('click', function (e) {
+        map.stopDirections();
+        var location = new Location(e.latlng.lat, e.latlng.lng);
+        location.getRoutesAndDisplay();
+    });
+
+    map.on('click', function (e) {
+        map.stopDirections();
+        var location = new Location(e.latlng.lat, e.latlng.lng);
+        location.getRoutesAndDisplay();
     });
 });
