80518ad217f9492a8c7def077373f6af33b485ad
David Amores <davidamores3@gmail.com>
2018-08-13 14:36:48 +1000

    Survey models, UI and logic started

--- survey/models.py
+++ survey/models.py
@@ -1,3 +1,54 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Survey(models.Model):
+    name = models.CharField(max_length=63)
+
+    def __str__(self):
+        return self.name
+
+
+class ChoiceGroup(models.Model):
+    name = models.CharField(max_length=31)
+
+    def __str__(self):
+        return self.name
+
+
+class Choice(models.Model):
+    group = models.ForeignKey(ChoiceGroup, on_delete=models.CASCADE)
+    text = models.CharField(max_length=31)
+    order = models.IntegerField(blank=True, null=True)
+
+    def __str__(self):
+        return '%s (%s)' % (self.text, self.group)
+
+
+class Question(models.Model):
+    text = models.TextField()
+    survey = models.ForeignKey(Survey, on_delete=models.CASCADE)
+    order = models.IntegerField(blank=True, default=0)
+    TYPE_CHOICES = (
+        ('Text', 'Text'),
+        ('SingleChoice', 'SingleChoice'),
+    )
+    type = models.CharField(
+        max_length=31,
+        choices=TYPE_CHOICES,
+        default='Text',
+    )
+    choices = models.ForeignKey(ChoiceGroup, on_delete=models.CASCADE, blank=True, null=True)
+
+    def __str__(self):
+        return '(%s) %s...' % (self.survey, self.text[:20])
+
+    def get_choices(self):
+        return Choice.objects.filter(group=self.choices).order_by('order')
+
+
+class Answer(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    date_created = models.DateTimeField(auto_now_add=True)
+    text = models.TextField()
--- survey/views.py
+++ survey/views.py
@@ -1,5 +1,30 @@
 from django.shortcuts import render
+from .models import Question, Answer
+from django.contrib import messages
 
 
 def index(request):
-    return render(request, 'survey/survey.html')
+    if request.method == 'GET':
+        if request.user.is_authenticated:
+            answers = Answer.objects.filter(user=request.user, question__survey__name='Post evacuation')
+            if answers:
+                messages.add_message(request, messages.INFO, "Survey already taken")
+                return render(request, 'survey/survey.html')
+            # survey = list(TextQuestion.objects.filter(survey__name='Post evacuation'))
+            # survey += list(ChoicesQuestion.objects.filter(survey__name='Post evacuation'))
+            # survey.sort(key=lambda x: x.order)
+            survey = Question.objects.filter(survey__name='Post evacuation').order_by('order')
+            context = {'survey': survey}
+            return render(request, 'survey/survey.html', context)
+        return render(request, 'evacuation/login.html')
+    else:
+        # email = request.POST['email']
+        # password = request.POST['password']
+
+        for question_id, answer in request.POST.items():
+            if 'q_' in question_id:
+                question_id = int(question_id[2:])
+                question = Question.objects.get(pk=question_id)
+
+        messages.add_message(request, messages.SUCCESS, "Answers submitted successfully")
+        return render(request, 'survey/survey.html')
