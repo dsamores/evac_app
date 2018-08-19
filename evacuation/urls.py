e5b8b6242a5ff8dd94ff25b171f2caf803cd7a73
David Amores <davidamores3@gmail.com>
2018-08-19 16:23:11 +1000

    Further nginx

--- evacuation/urls.py
+++ evacuation/urls.py
@@ -13,6 +13,7 @@ urlpatterns = [
     path('alerts', views.alerts, name='alerts'),
     path('statements', views.statements, name='statements'),
     path('walkthrough', views.walkthrough, name='walkthrough'),
+    path('health-check', views.browser_login, name='health-check'),
 
     # web services
     path('new_user', views.new_user, name='new_user'),
