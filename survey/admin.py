dc3125e27d2a111b808eb10130fa691cc603ffd6
David Amores <davidamores3@gmail.com>
2018-09-06 16:52:09 +1000

    Improved admin view for surveys

--- survey/admin.py
+++ survey/admin.py
@@ -3,6 +3,16 @@ from django.contrib import admin
 from .models import Survey, Question, ChoiceGroup, Choice
 
 admin.site.register(Survey)
-admin.site.register(Question)
 admin.site.register(ChoiceGroup)
 admin.site.register(Choice)
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ('display_name', 'survey')
+    list_filter = ('survey__name',)
+
+    def display_name(self, obj):
+        return '%s...' % obj.text[:20]
+
+
+admin.site.register(Question, QuestionAdmin)
