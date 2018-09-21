259b6dbd290866631838041d78f10b4bcc08deb7
David Amores <davidamores3@gmail.com>
2018-09-21 11:52:04 +1000

    Added fixture for every place including landmarks

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -12,7 +12,7 @@ done
 
 echo "Apply database migrations."
 python3 manage.py migrate
-python3 manage.py loaddata exits_and_assembly.json
+python3 manage.py loaddata places.json
 python3 manage.py loaddata offices_and_desks.json
 
 echo "Run collectstatic"
