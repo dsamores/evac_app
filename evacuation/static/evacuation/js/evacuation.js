fe04c535f65d976aa69fc349d056110d8ebfc968
David Amores <davidamores3@gmail.com>
2018-09-23 22:54:40 +1000

    Save hidden/visible events

--- evacuation/static/evacuation/js/evacuation.js
+++ evacuation/static/evacuation/js/evacuation.js
@@ -148,3 +148,32 @@ function handleTouchEnd(evt) {
 $("#content a").click(function(){
     (new Interaction('link-click', $(this).attr('href'), window.location.href, null)).save();
 });
+
+$(document).ready(function($) {
+    var hidden, visibilityChange;
+    if (typeof document.hidden !== "undefined") { // Opera 12.10 and Firefox 18 and later support
+      hidden = "hidden";
+      visibilityChange = "visibilitychange";
+    } else if (typeof document.msHidden !== "undefined") {
+      hidden = "msHidden";
+      visibilityChange = "msvisibilitychange";
+    } else if (typeof document.webkitHidden !== "undefined") {
+      hidden = "webkitHidden";
+      visibilityChange = "webkitvisibilitychange";
+    }
+
+    function handleVisibilityChange() {
+      if (document[hidden]) {
+        (new Interaction('go-hidden', '', window.location.href, null)).save();
+      } else {
+        (new Interaction('go-visible', '', window.location.href, null)).save();
+      }
+    }
+
+    // Warn if the browser doesn't support addEventListener or the Page Visibility API
+    if (typeof document.addEventListener === "undefined" || hidden === undefined) {
+      console.log("This demo requires a browser, such as Google Chrome or Firefox, that supports the Page Visibility API.");
+    } else {
+      document.addEventListener(visibilityChange, handleVisibilityChange, false);
+    }
+});
