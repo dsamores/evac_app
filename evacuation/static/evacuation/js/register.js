7e39bb9ac20b883b29cec2c45b60cf816f39cb9a
David Amores <davidamores3@gmail.com>
2018-09-23 22:20:57 +1000

    Further registration validations

--- evacuation/static/evacuation/js/register.js
+++ evacuation/static/evacuation/js/register.js
@@ -4,8 +4,8 @@ $(document).ready(function($) {
         var email = $.trim($('#email').val());
 
         if (!/@unimelb.edu.au\s*$/.test(email) && !/@student.unimelb.edu.au\s*$/.test(email)) {
-           $('#email-error').text('Please enter a @unimelb.edu.au or @student.unimelb.edu.au email');
            $('#email').addClass('is-invalid');
+           alert("Please check the form errors");
            return false;
         }
         return true;
